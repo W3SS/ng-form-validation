79f73b77d6c2c0fc4020b3c91aa3998d0716576d
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-19 21:42:47 -0500

    added the contact model

---
+++ src/app/model-form/contact.model.ts
@@ -0,0 +1,9 @@
+/* tslint:disable*/
+export interface Contact {
+    id: string;
+    company: string;
+    contactName: string;
+    email: string;
+    leadTitle: string;
+    phoneNumber: string;
+}
